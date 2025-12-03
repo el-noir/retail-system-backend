09de05323d39c5f40d04d62381791013e33e33b8
HassanAli206 <hali83418@gmail.com>
2025-12-03 09:55:54 +0500

    started setting up

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from 'src/prisma.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
+    const mockPrisma = { user: { findUnique: jest.fn(), create: jest.fn() } } as unknown as PrismaService;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: mockPrisma },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);

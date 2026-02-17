e43e334c5c37c1b91b7c23a8a57bcc0838f3eba7
el-noir <mudasirshah9777@gmail.com>
2026-02-18 02:44:26 +0500

    Simplify PrismaService to minimal recommended version

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,29 +1,13 @@
-
-import { Injectable, OnModuleInit, OnModuleDestroy, Logger } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  private readonly logger = new Logger(PrismaService.name);
-
-  constructor() {
-    super({
-      log: ['error', 'warn'],
-    });
-  }
-
   async onModuleInit() {
-    try {
-      await this.$connect();
-      this.logger.log('Database connection established');
-    } catch (error) {
-      this.logger.error('Database connection failed', error as Error);
-      throw error;
-    }
+    await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
-    this.logger.log('Database connection closed');
   }
 }
